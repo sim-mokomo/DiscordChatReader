5dbe52e7484d729ceaee4a4961792dfdc6c112fd
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2022-01-16 17:32:23 +0900

    fix: config loaderの実相をjsonの構成に合わせて修正。readmeも同様に修正

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 
 チャンネルにBotを招待すると、自身が打ち込んだ文字を読み上げてくれる。  
-個人利用用なので読み上げ対象として設定できる人間は1人のみ。
+読み上げ対象は複数人指定できる。
 
 # 環境
 
@@ -24,13 +24,14 @@ pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl
 `<root>/config-template.json` を `<root>/config.json` に変更することで読み込まれるようになる。
 デフォルトではダミー値が記述されているので、以下の説明を参考に適切な値を `config.json` に入力する。
 
-- speaker_member_id
-  - Discordのボイスチャンネル上のアイコンから「IDをコピー」で取得
 - token
   - Discordが発行するBotトークン
-- voice_text
-  - api_key
-    - VoiceTextWebApi登録時に発行されるAPIキー
-  - speaker
+- voice_text_web_api_key
+  - VoiceTextWebApi登録時に発行されるAPIキー  
+- speaker_convert_config_table
+  - member_id
+    - 読み上げ対象のID
+      - Discordのボイスチャンネル上のアイコンから「IDをコピー」で取得
+  - speaker_name
   - speed
     - VoiceTextWebApiの「APIマニュアル」を参考に記述
--- src/config_loader.py
+++ src/config_loader.py
@@ -11,19 +11,15 @@ class ConfigLoader:
         json_open = open('config.json', 'r')
         json_obj = json.load(json_open)
         config.token = json_obj['token']
+        config.voice_text_web_api_key = str(json_obj['voice_text_web_api_key'])
+
         print(json_obj)
         speaker_convert_config_table_json = json_obj['speaker_convert_config_table']
         for speaker_convert_config_json in speaker_convert_config_table_json:
             speaker_config = SpeakerConverterConfig()
             speaker_config.member_id = int(speaker_convert_config_json['member_id'])
             speaker_config.speaker_name = str(speaker_convert_config_json['speaker_name'])
+            speaker_config.speed = int(speaker_convert_config_json['speed'])
             config.speaker_convert_config_table.append(speaker_config)
 
-        voice_text_json_obj = json_obj['voice_text']
-        config.voice_text.api_key = voice_text_json_obj['api_key']
-        config.voice_text.speed = voice_text_json_obj['speed']
-
         return config
-
-
-print(ConfigLoader.load("config.json"))
